9ca7ee65c479f52381c726e68069645a9ac0079e
TerryDutton <t_dutton85@hotmail.com>
2022-03-09 07:57:49 +0000

    Created computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("All working!");
+const Game = new (function(){
+
+    const ROCK = "rock";
+    const PAPER = "paper";
+    const SCISSORS = "scissors";
+
+    this.computerPlay = function(){
+        const random = Math.floor(Math.random() * 3);
+        return [ROCK, PAPER, SCISSORS][random];
+    }
+})();
+
+console.log(Game.computerPlay());
