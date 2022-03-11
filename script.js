974748fcd5719c8daad8797f1a1c8699f9c9cd42
TerryDutton <t_dutton85@hotmail.com>
2022-03-11 08:36:10 +0000

    Refactored RockPaperScissors to contain only game rules and not UI rules

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const RockPaperScissors = new (function(){
     const PAPER = "paper";
     const SCISSORS = "scissors";
 
+    this.ROCK = ROCK;
+    this.PAPER = PAPER;
+    this.SCISSORS = SCISSORS;
+
     const choices = [ROCK, PAPER, SCISSORS];
 
     const whatDoesThisBeat = {
@@ -12,40 +16,21 @@ const RockPaperScissors = new (function(){
         [SCISSORS]: PAPER
     };
 
-    const computerPlay = function(){
+    this.isInputValid = input => choices.includes(input);
+
+    this.getRandomChoice = () => {
         const random = Math.floor(Math.random() * 3);
         return choices[random];
     };
 
-    const sanitizeInput = function(input){
-        const i = input.toLowerCase();
-        const isValid = choices.includes(i);
-        return [i, isValid];
-    };
-
-    const determineVictor = function(selection_player1, selection_player2){
-        return {
+    this.determineVictor = (selection_player1, selection_player2) => {
+        const isValid_Input1 = this.isInputValid(selection_player1);
+        const isValid_Input2 = this.isInputValid(selection_player2);
+        
+        if (isValid_Input1 && isValid_Input2) return {
             p1_victory: whatDoesThisBeat[selection_player1] === selection_player2,
             p2_victory: whatDoesThisBeat[selection_player2] === selection_player1
         };
-    };
-
-    const getChoiceFromPlayer = function(player){
-        while(true){
-            const input = prompt(`Player ${player}, what do you choose?`);
-            const [choice, isValid] = sanitizeInput(input);
-            if(!isValid) alert(`'${input}' is not a valid choice ):`);
-            else return choice;
-        }
-    };
-
-    const getPlayerChoices = function(is2Player){
-        const p1Choice = getChoiceFromPlayer(1);
-        const p2Choice = is2Player ? getChoiceFromPlayer(2) : computerPlay();
-        return {p1Choice, p2Choice};
-    };
-
-    this.play = function({is2Player = false, rounds = 5}){
-
+        else throw new TypeError("Inputs were invalid");
     };
 })();
